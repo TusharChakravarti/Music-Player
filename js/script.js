acdff91440b5ada5a10781f0755e17ee454612ed
Tushar Chakravarti <tusharch04092005@gmail.com>
2026-03-04 23:41:32 +0530

    spotify clone

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ for (const song of songs) {
             <img class="invert" src="/img/music.svg" alt="">
             <div class="info">
  <div class="songname">${decodeURIComponent(song)}</div>
-          <div class="songartist">Harry</div>
+          <div class="songartist">xyz</div>
         
             </div>
              <div class="playNow">
@@ -110,24 +110,64 @@ function secondToMinutesSecond(seconds){
 }
 
 async function displayAlbum() {
-  let a = await fetch(`http://127.0.0.1:5500/music/`)
+  let a = await fetch(`http://127.0.0.1:5500/songs/`)
 let response =  await a.text();
-let folder;
+
 let div = document.createElement('div')
 div.innerHTML = response
 let anchors = div.getElementsByTagName('a')
 
-Array.from(anchors).forEach(e=>{
+let array  = Array.from(anchors)
+
+for (let index = 0; index < array.length; index++) {
+  const e = array[index];
+  
 
   
-  if(e.href.includes('/music/')){
+  if(e.href.includes('/songs/')){
     console.log(e.href);
     
-    console.log(e.href.split('/').slice(-1)[0]);
-    
+    let folder = e.href.split('/').slice(-1)[0];
+    // get the meta data of the folder
+      let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+let response =  await a.json();
+console.log(response);
+
+document.querySelector('.cardContainer').innerHTML = document.querySelector('.cardContainer').innerHTML+
+ `<div class="card" data-folder="${folder}">
+                 
+                 <img aria-hidden="false" draggable="false" loading="lazy" src="/songs/${folder}/cover.jpg" data-testid="card-image" alt="" class="LBM25IAoFtd0wh7k3EGM Z3N2sU3PRuY4NgvdEz55 DlkUu3oBOxXLc2LtOd3N PgTMmU2Gn7AESFMYhw4i">
+                 <div class="playbutton">
+                 <svg data-encore-id="icon" role="img" aria-hidden="true" class="e-91000-icon e-91000-baseline" viewBox="0 0 24 24"><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606"></path></svg>
+                 </div>
+               
+                    <h2>${response.title}</h2>
+                    <p>${response.description}</p>
+                </div>`
+
   }
 
+
+
+
+  
+}
+  
+
+  //Load the playlist whenever card is clicked
+Array.from(document.getElementsByClassName('card')).forEach(function(e){
+  
+  e.addEventListener('click',async (item)=>{
+    console.log(e.dataset.folder,item.target,item.currentTarget.dataset);
+    
+  await getSongs(`songs/${item.currentTarget.dataset.folder}`);
+   playMusic(songs[0])
+
+  })
+
+
 })
+
 
 
 
@@ -138,7 +178,7 @@ async function main(){
    
     
     // Get the list of  all the songs 
-  await getSongs('music/ncs')
+  await getSongs('songs/ncs')
  playMusic(songs[0],true);
  
 // Display the albums on the page
@@ -242,30 +282,72 @@ let seek = (currentSong.currentTime / currentSong.duration)*100
      
   })
 
-  // add event listner
-  document.querySelector('.range input').addEventListener('change',function(e){
+  // add event listner to volume
+  document.querySelector('.range').getElementsByTagName('input')[0].addEventListener('change',function(e){
         console.log("Setting Value to ",e.target.value,"/  100");
      
         
         currentSong.volume =parseInt(e.target.value)/100
+        if(currentSong.volume>0){
+ document.querySelector('.volume > img').src = '/img/volume.svg'
+        }
+        else {
+ document.querySelector('.volume > img').src = '/img/mute.svg'
+        }
+       
         
 
   })
 
-  //Load the playlist whenever card is clicked
-Array.from(document.getElementsByClassName('card')).forEach(function(e){
-  
-  e.addEventListener('click',async (item)=>{
-    console.log(e.dataset.folder,item.target,item.currentTarget.dataset);
-    
-  await getSongs(`music/${item.currentTarget.dataset.folder}`);
+  // add event listner to mute
 
+  document.querySelector('.volume > img').addEventListener('click',(e)=>{
+   console.log(e.target.src);
+   
+    
+      // if(e.target.src.includes('/img/volume.svg')){
+        
+        
+      //   // e.target.src = e.target.src.replace('/img/volume.svg','/img/mute.svg')
+      //    e.target.src = "/img/mute.svg";
+      //   console.log(e.target.src);
+        
+      //   currentSong.volume = 0;
+      //    document.querySelector('.range').getElementsByTagName('input')[0].value = 0
+        
+
+      // }
+      // else{
+          
+      //   //e.target.src =   e.target.src.replace('/img/mute.svg','/img/volume.svg')
+      //     e.target.src = "/img/volume.svg";
+      //    currentSong.volume = 0.1
+      //     document.querySelector('.range').getElementsByTagName('input')[0].value = 10
+
+      // }
+
+      
+      if(currentSong.muted){
+         currentSong.muted = false;
+        
+         e.target.src = "/img/volume.svg";
+     document.querySelector('.range').getElementsByTagName('input')[0].value =  currentSong.volume*100;
+     
+
+      }
+      else{
+          
+  e.target.src = "/img/mute.svg";
+        
+        currentSong.muted = true;
+     document.querySelector('.range').getElementsByTagName('input')[0].value = 0
+
+     
+      }
   })
 
 
-})
 
-  
 
 
 
